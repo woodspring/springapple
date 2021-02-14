9bb4b5d1f4c6bfcdb1bef5b9d4725ebc150b9472
woodspring.toronto <woodspring.toronto@gmail.com>
2021-02-14 09:44:15 -0500

    add controller

--- src/main/java/woodspring/springapple/controller/AppleController.java
+++ src/main/java/woodspring/springapple/controller/AppleController.java
@@ -1,5 +1,28 @@
 package woodspring.springapple.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import woodspring.springapple.entities.FXSpot;
+
+import woodspring.springapple.Service.impl.AppleServiceImpl;
+
+@Controller
+@RequestMapping(value="/apple/")
 public class AppleController {
+	
+	@Autowired
+	AppleServiceImpl appleImpl;
+	
+	@GetMapping("listFX") 
+	@ResponseBody
+	public List<FXSpot> getFXSpot() {
+		 return appleImpl.getFxQoute();
+		
+	}
 
 }
